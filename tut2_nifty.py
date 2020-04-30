5223154de0c88e277d7c78e1970eb6b24a652890
Tanay Singhania <singhaniatanay18@gmail.com>
2020-04-30 18:56:59 +0530

    imported data from Yahoo

--- tut2_nifty.py
+++ tut2_nifty.py
@@ -1,6 +1,10 @@
 import bs4 as bs
 import pickle
 import requests
+import os
+import datetime as dt
+import pandas_datareader.data as web
+
 
 def getNifty():
     resp = requests.get('https://en.wikipedia.org/wiki/NIFTY_50')
@@ -16,6 +20,32 @@ def getNifty():
     with open("nifty50tickers.pickle","wb") as f:
         pickle.dump(tickers,f)
 
+    return tickers
+
 #getNifty()
 
 def get_data_from_yahoo(reload_nifty=False):
+    if reload_nifty:
+        tickers = getNifty()
+    else:
+        with open("nifty50tickers.pickle","rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2010, 1, 1)
+    end = dt.datetime.now()
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker,'yahoo',start,end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already Have {}'.format(ticker))
+
+
+
+
+get_data_from_yahoo()
