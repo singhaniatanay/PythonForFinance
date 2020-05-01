0a8288061dda8e9ac554c73c25a5bc3463223d95
Tanay Singhania <singhaniatanay18@gmail.com>
2020-05-01 18:30:24 +0530

    Extracted Features and Applieed Voting Classifier with Random Forest

--- tut4.py
+++ tut4.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
 import pickle
+from collections import Counter
+
+from sklearn import svm, neighbors
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 
 def process_data_for_labels(ticker):
     days_future = 7
@@ -14,4 +19,63 @@ def process_data_for_labels(ticker):
     df.fillna(0,inplace=True)
     return tickers,df
 
-process_data_for_labels('BAJAJ-AUTO.NS')
+
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > requirement:
+            return 1
+        if col < -requirement:
+            return -1
+
+    return 0
+
+
+def extract_featuresets(ticker):
+    tickers,df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold,
+                                                df['{}_1d'.format(ticker)],
+                                                df['{}_2d'.format(ticker)],
+                                                df['{}_3d'.format(ticker)],
+                                                df['{}_4d'.format(ticker)],
+                                                df['{}_5d'.format(ticker)],
+                                                df['{}_6d'.format(ticker)],
+                                                df['{}_7d'.format(ticker)] ))
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:',Counter(str_vals))
+    df.fillna(0,inplace=True)
+
+    df = df.replace([np.inf,-np.inf],np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df.replace([np.inf,-np.inf],0)
+    df_vals.fillna(0,inplace=True)
+
+    X,y = df_vals.values , df['{}_target'.format(ticker)].values
+
+    return X,y,df
+
+
+
+def ML_on_finance_data(ticker):
+    X,y,df = extract_featuresets(ticker)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
+
+
+    # clf = neighbors.KNeighborsClassifier()
+    clf = VotingClassifier([('lsvc',svm.LinearSVC()),
+                            ('knn',neighbors.KNeighborsClassifier()),
+                            ('rfor',RandomForestClassifier())])
+    clf.fit(X_train,y_train)
+    confidence = clf.score(X_test,y_test)
+    predictions = clf.predict(X_test)
+
+    print('Predicted spread :',Counter(predictions))
+    print('Accuracy:',confidence)
+    return confidence
+
+ML_on_finance_data('BAJAJFINSV.NS')
